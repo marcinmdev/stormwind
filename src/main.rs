eb83674f68a3598790178f52a4206cf3a78a94d0
marcinmdev <marcin@insanet.pl>
2024-12-23 13:26:36 +0100

    request param fix - is_day

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,7 @@ fn main() {
 
     let url = format!(
         "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}\
-        &current=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,\
+        &current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,\
         rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,\
         wind_speed_10m,wind_direction_10m,wind_gusts_10m&\
         temperature_unit={}&wind_speed_unit={}&precipitation_unit={}",
